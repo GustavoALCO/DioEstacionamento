14e0c70c74ea34ae2c8ba96285f48b26fdaa0c85
Gustavo Alves <119056348+GustavoALCO@users.noreply.github.com>
2023-07-23 16:02:53 -0300

    primeiro commit

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace Estacionamento.Models
+{
+    public class Veiculo
+    {
+        private double precoInicial = 0;
+        private double precoPorHora = 0;
+        private List<string> lista = new List<string>();
+
+        public void adicionarVeiculo()
+        {
+            Console.WriteLine("Escreva o nome da placa do veiculo para estacionar");
+            lista.Add(Console.ReadLine());
+        }
+        
+        public void removerVeiculo()
+        {
+            Console.WriteLine("qual a placa do veiculo que deseja retirar");
+            string placa = Console.ReadLine();
+
+            // verificar se existe a placa na lista 
+            if (lista.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas em que o veiculo ficou estacionado : ");
+                double horas = Convert.ToDouble(Console.ReadLine());
+               double precoTotal = precoInicial + (horas * precoPorHora);
+                lista.Remove(placa);
+                Console.WriteLine($"O valor que o veiculo da Placa {placa} será de: {precoTotal}");
+            
+        }
+            else
+                Console.WriteLine("Desculpe, placa não encontrada");
+        }
+        public void MostrarPlacas ()
+        {
+            Console.WriteLine("Placas de carros estacionado no momento");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+        }
+        public Veiculo (double precoinicial,double precoHora )
+        {
+            this.precoInicial = precoinicial;
+            this.precoPorHora = precoHora;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Estacionamento.Models;
+
+namespace Estacionamento
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("---------------------------------------\n         Sistema do estacionamento \n---------------------------------------\n\nDigite o preço inicial:");
+            double precoInicial = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor adicional cobrado por hora: ");
+            double precoHora = double.Parse(Console.ReadLine());
+
+            Veiculo veiculo = new Veiculo(precoInicial, precoHora);
+
+           bool repeticao = true;
+          while (repeticao)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a sua opção :\n" +
+                    "1 - cadastrar veiculo\n" +
+                    "2 - remover veiculo \n" +
+                    "3 - lista de veiculos\n" +
+                    "4 - sair");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        veiculo.adicionarVeiculo(); 
+                        break;
+
+                    case "2":
+                        veiculo.removerVeiculo();
+                        break;
+
+                    case "3":
+                        veiculo.MostrarPlacas();
+                        break;
+                    case "4":
+                        repeticao = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("opção invalida");
+                        break;
+                }
+                Console.WriteLine("Pressione Enter para continuar...");
+                Console.ReadLine();
+            }
+                
+            
+        }
+    }
+}
